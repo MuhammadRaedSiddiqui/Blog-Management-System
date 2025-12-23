15a9285106419be15b66be246ce01c56a726236f
MuhammadRaedSiddiquie <bhairaed636@gmail.com>
2025-12-23 08:19:16 +0500

    fix vercel deployment issue

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'os8zbf04h6.ufs.sh',
+      },
+    ],
+  },
   serverExternalPackages: ['@prisma/client', 'prisma'],
   // Disable Turbopack due to Prisma symlink issues on Windows
   bundlePagesRouterDependencies: true,
